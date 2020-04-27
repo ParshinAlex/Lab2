3d9e03d13c307ba3a3842fed504001d7e3d1a59a
AlexParshyn <parshin_o@ukr.net>
2020-04-28 00:17:44 +0300

    Reformation after defense
    This changes were made after the defense of the laboratory work

--- VHI_Index_Code.py
+++ VHI_Index_Code.py
@@ -1,4 +1,3 @@
-import urllib.request
 import pandas as pd
 import os, os.path
 import glob
@@ -6,31 +5,37 @@ import requests
 import datetime
 
 main_folder = "D:\Python\Lab2\collected_data"
-pd.options.display.max_rows = 50000
-minYear = 2005
+minYear = 1982
 maxYear = 2020
 
-changedIndex = {1:24, 2:25, 3:5, 4:6, 5:27, 6:23, 7:26, 8:7, 9:11, 10:13, 11:14, 12:15, 13:16, 14:17, 15:18, 16:19, 17:21,
-        18:22, 19:8, 20:9, 21:10, 22:1, 23:3, 24:2, 25:4, 26:12, 27:20}
+
+# Accessory functions
 
 def clean_data(directory, index):
     os.chdir(directory)
-    for file in glob.glob(os.path.join(directory, "province{}*.csv".format(index))):
+    for file in glob.glob(os.path.join(directory, "province_{}*.csv".format(index))):
         os.remove(file)
+
 
 def get_index():
     print('Please, insert index of a province: ')
     index = int(input())
     while True:
-        if ((index > 27) or (index < 1)): print('Invalid input. Please, try again:')
-        else: return index
+        if (index > 27) or (index < 1):
+            print('Invalid input. Please, try again:')
+        else:
+            return index
+
 
 def get_year():
     print('Please, insert year: ')
     year = int(input())
     while True:
-        if ((year < minYear) or (year > maxYear)): print('Invalid input. Please, try again:')
-        else: return year
+        if (year < minYear) or (year > maxYear):
+            print('Invalid input. Please, try again:')
+        else:
+            return year
+
 
 def prepare_data(line):
     if '<pre>' in line:
@@ -42,10 +47,15 @@ def prepare_data(line):
         return ''
     return (line + '\n')
 
+
+# Downloading functions
+
 def download_province(folder, index):
-    url = "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_admin.php?country=UKR&provinceID={}&year1={}&year2={}&type=Mean".format(changedIndex[index], minYear, maxYear)
+    url = "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_admin.php?country=UKR&provinceID={}&year1={}&year2={}&type=Mean".format(
+        index, minYear, maxYear)
     response = requests.get(url)
-    path = os.path.join(folder, "province{}_{}.csv".format(index, datetime.datetime.today().strftime("%d-%m-%Y_%H-%M")))
+    path = os.path.join(folder,
+                        "province_{}_{}.csv".format(index, datetime.datetime.today().strftime("%d-%m-%Y %H-%M")))
     with open(path, 'w') as file:
         file.write("Year,Week,SMN,SMT,VCI,TCI,VHI\n")
         for line in response.iter_lines(chunk_size=512, decode_unicode=True):
@@ -53,87 +63,52 @@ def download_province(folder, index):
         file.close()
     print('Province {} is ready to use.'.format(index))
 
+
 def download_all_provinces():
-    for i in range (1, 28):
+    for i in range(1, 28):
         clean_data(main_folder, i)
         download_province(main_folder, i)
 
+
+# Getting/Printing DataFrame functions
+
 def get_df(folder, index):
-    df = pd.DataFrame(columns=['Year','Week', 'SMN', 'SMT', 'VCI', 'TCI', 'VHI'])
+    df = pd.DataFrame(columns=['Year', 'Week', 'SMN', 'SMT', 'VCI', 'TCI', 'VHI'])
     os.chdir(folder)
-    csv_path = glob.glob('province{}*.csv'.format(index))
-    df = pd.read_csv(csv_path[0], index_col= False)
-    df['provinceID'] = index
-    return(df)
+    csv_path = glob.glob('province_{}_*.csv'.format(index))
+    df = pd.read_csv(csv_path[0], index_col=False)
+    # df['Province'] = index     # Remove / add id of a province
+    return df
+
 
 def get_merged_df(folder):
     df = pd.DataFrame(columns=['Year', 'Week', 'SMN', 'SMT', 'VCI', 'TCI', 'VHI'])
     os.chdir(folder)
-    csv_paths = glob.glob('province*.csv')
-    dfs = []
-    for path in csv_paths:
-        dfs_x = pd.read_csv(path, index_col=False)
-        dfs_x['provinceID'] = path.split("_")[0][8:]
-        dfs.append(dfs_x)
-    df = pd.concat(dfs)
-    return(df)
+    for file in glob.glob(os.path.join(folder, "province_*.csv")):
+        temp = pd.read_csv(file, header=0, index_col=False)
+        # temp['Province'] = file.split('_')[2]      # Remove / add id of a province
+        df = df.append(temp, ignore_index=True)
+    return df
 
-def vhi_province(folder):
-    index = get_index()
-    year = get_year()
-    df = get_df(folder, index)
-    df = df[['Year', 'Week', 'VHI']]
-    df = df[df['Year'] == year]
-    print_df(df)
-    print('Maximum value of VHI:')
-    print(df['VHI'].max())
-    print('Minimum value of VHI: ')
-    print(df['VHI'].min())
-    print('Average value of VHI: ')
-    print(df['VHI'].mean())
-
-def vhi_province_critical_droughts(folder):
-    index = get_index()
-    df = get_df(folder, index)
-    df = df[['Year', 'Week', 'VHI']]
-    modded_df = df[df['VHI'] < 15][['Year', 'Week', 'VHI']]
-    modded_df = modded_df.groupby(['Year']).mean()
-    print_df(modded_df)
-
-def vhi_province_temperate_droughts(folder):
-    index = get_index()
-    df = get_df(folder, index)
-    df = df[['Year', 'Week', 'VHI']]
-    modded_df = df[(df['VHI'] < 35) & (df['VHI'] > 15)][['Year', 'Week', 'VHI']]
-    modded_df = modded_df.groupby(['Year']).mean()
-    print_df(modded_df)
-
-def custom_function(folder):
-    #index = get_index()
-    #df = get_df(folder, index)
-    df = get_merged_df()
-    modded_df = df['VHI'].max()
 
 def print_df(dataframe):
     print(dataframe)
 
+# Main function
+
 def main():
-    for i in range (1, 28): clean_data(main_folder, i)
     while True:
         print('----------------------------------------------')
-        print('This is the main menu of a Laboratory Work 1.')
-        print('1. Download one province')
-        print('2. Download all provinces')
-        print('3. Create and print DataFrame of one province')
-        print('4. Create and print DataFrame of all provinces')
-        print('5. VHI for province (1 year), min / max / average values')
-        print('6. VHI for province (all years), critical droughts')
-        print('7. VHI for province (all years), temperate droughts')
-        print('8. Custom function')
-        print('0. Exit')
-        print('Please, choose number: ')
+        print('This is the main menu of a Laboratory Work 1. \n'
+              '1. Download one province \n'
+              '2. Download all provinces \n'
+              '3. Create and print DataFrame of one province \n'
+              '4. Create and print DataFrame of all provinces\n'
+              '0. Exit\n'
+              'Please, choose number: \n')
         key = int(input())
-        if key == 0: break
+        if key == 0:
+            break
         elif key == 1:
             index = get_index()
             download_province(main_folder, index)
@@ -144,15 +119,9 @@ def main():
             print_df(get_df(main_folder, index))
         elif key == 4:
             print_df(get_merged_df(main_folder))
-        elif key == 5:
-            vhi_province(main_folder)
-        elif key == 6:
-            vhi_province_critical_droughts(main_folder)
-        elif key == 7:
-            vhi_province_temperate_droughts(main_folder)
-        elif key == 8:
-            custom_function(main_folder)
-        else: print('Invalid input. Please, try again.')
+        else:
+            print('Invalid input. Please, try again.')
+
 
 if __name__ == '__main__':
     main()
